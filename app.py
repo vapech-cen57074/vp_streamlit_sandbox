46862b579708a2116ece8b9f926af21d9b271ae5
Vaclav Pech <vapech@csas.cz>
2024-05-24 10:55:40 +0200

    trying pandasai

--- app.py
+++ app.py
@@ -1,70 +1,29 @@
 import streamlit as st
 import pandas as pd
-from langchain_openai import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain_core.runnables import RunnableLambda
+from pandasai import SmartDataframe
+from pandasai.llm import OpenAI
 
-@st.cache_data
-def load_data():
-    data = pd.read_csv('data/titanic.csv')
-    return data
 
-def get_insights(question, data):
-    # Define your prompt template
-    template = """
-    The dataset contains information about passengers on the Titanic. 
-    The columns are: {columns}.
-    Here is the first few rows of the dataset: {data_head}
-    
-    Question: {question}
-    Answer:
-    """
-    
-    # Prepare the input for the prompt
-    columns = ", ".join(data.columns)
-    data_head = data.head().to_dict(orient='records')
-    input_data = {
-        "columns": columns,
-        "data_head": data_head,
-        "question": question
-    }
-    
-    prompt = PromptTemplate(template=template, input_variables=["columns", "data_head", "question"])
+data = pd.read_csv('data/titanic.csv')
 
-    # Generate response using OpenAI
+st.title("Chat with Titanic Dataset")
+st.write("Here is the Titanic dataset:")
+
+with st.expander("Dataframe Preview"):
+    st.write(data.head(7))
+
+
+
+
+
+
+query = st.text_area("Chat with Dataframe")
+st.write(query)
+
+if query:
     llm = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-    formatted_prompt = prompt.format(**input_data)
-    
-    # Create a RunnableLambda with the llm callable
-    runnable = RunnableLambda(lambda x: llm(x))
-    
-    # Execute the runnable using the invoke method
-    result = runnable.invoke(formatted_prompt)
-    
-    return result
+    query_engine = SmartDataframe(data, config={"llm":llm})
 
-# Streamlit App
-def main():
-    st.title("Chat with Titanic Dataset")
-    
-    # Load data
-    data = load_data()
-    
-    # Display data
-    st.write("Here is the Titanic dataset:")
-    st.write(data)
-    
-    # Input for user's question
-    question = st.text_input("Ask a question about the dataset:")
-    
-    if question:
-        st.write("You asked:", question)
-        
-        # Get insights
-        insights = get_insights(question, data)
-        
-        # Display insights
-        st.write("Insight:", insights)
+    answer = query_engine.chat(query)
+    st.write(answer)
 
-if __name__ == "__main__":
-    main()
